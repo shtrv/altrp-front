09d15765a2260995434675d1bbb6038d165f30b2
shtrv <71714403+shtrv@users.noreply.github.com>
2024-06-10 12:29:18 +0300

    new action

---
+++ kadastr_popup/refresh_after_save.js
@@ -0,0 +1,7 @@
+// Получаем текущий URL
+var currentUrl = window.location.href;
+
+// Проверяем, находится ли пользователь на странице obekti-arendi
+if (currentUrl.includes('obekti-arendi')) {
+    // Выполняем код для страницы obekti-arendi
+    RaiseActions('_refresh_table_');}
