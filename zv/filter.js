4ecb6ae0d6e8d7ce065ebf04d6ef7df3274a2ed7
shtrv <71714403+shtrv@users.noreply.github.com>
2024-06-18 16:45:04 +0300

    new filter

---
+++ zv/filter.js
@@ -0,0 +1,13 @@
+var filter = altrpHelpers.getDataByPath("altrpforms.filter");
+
+Object.keys(filter).forEach(key => {
+    // Проверяем, что значение свойства не пустое (не null и не undefined)
+    if (filter[key] !== null && filter[key] !== undefined && filter[key] !== '') {
+      // Вызываем setPageParams для непустого свойства
+      setPageParams(key, filter[key]);
+    }
+  });
+
+var t = altrpHelpers.getDataByPath('altrppagestate.altdata.tables');
+Object.keys(t).forEach(id=>t[id].context.updateData());	//Рефреш таблицы
+  
