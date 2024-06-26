9986438c6eb795fa01c397454f99d8adb3194220
shtrv <71714403+shtrv@users.noreply.github.com>
2024-06-26 13:54:13 +0300

    default

---
+++ prod/calc_price_and_len.js
@@ -0,0 +1,22 @@
+var map_segments = altrpHelpers.getDataByPath('altrppagestate.map_segments');
+var len = 0.0;
+map_segments.forEach(i=>len = len + (Number(i.length_passport ?? 0.0)));
+var wells = map_segments.filter(i=>!!i.layer.includes('wells')).length;
+if (len > 0){
+	var prevLenMap = altrpHelpers.getDataByPath('altrpforms.edit_form.length_by_map') ?? 0;
+	len = len.toFixed(2);
+	altrpHelpers.setDataByPath('altrpforms.edit_form.length_by_map', len);
+	if (prevLenMap == altrpHelpers.getDataByPath('altrpforms.edit_form.length')) //одинаковые, значит человек не трогал
+		altrpHelpers.setDataByPath('altrpforms.edit_form.length', len);
+}
+altrpHelpers.setDataByPath('altrpforms.edit_form.wells_count', wells);
+altrpHelpers.getComponentByElementId('_objects_list_').updateElement();
+
+var prices = altrpHelpers.getDataByPath('altrpdata.prices');
+var frm = altrpHelpers.getDataByPath('altrpforms.edit_form');
+var price;
+if (frm.max_diameter > 0 && frm.laying_type > 0 && frm.length > 0 && frm.method_id == 2192){
+    price = prices.filter(i=>i.laying_type_id == frm.laying_type && i.min_diameter < frm.max_diameter && i.max_diameter >= frm.max_diameter)[0]?.price * frm.length / 1000.0;
+	altrpHelpers.setDataByPath('altrpforms.edit_form.price', price?.toFixed(2));
+	altrpHelpers.setDataByPath('altrpforms.edit_form.price_is_autocalculated', (price > 0 ? 1 : 0));
+}
