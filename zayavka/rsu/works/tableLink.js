1c5cd2ae48b88be384b50a9a6ebcdf45b85cf6e7
shtrv <71714403+shtrv@users.noreply.github.com>
2024-09-16 11:13:29 +0300

    added jslinks

---
+++ zayavka/rsu/works/tableLink.js
@@ -0,0 +1,3 @@
+javascript:
+altrpHelpers.setDataByPath('altrppagestate._list__current_row', altrpHelpers.getDataByPath('altrpdata.works')[{{altrpIndex}}]);
+RaiseActions('hidden_row_click');
---
+++ zayavka/rsu/works/tableOBJLink.js
@@ -0,0 +1,3 @@
+javascript:
+altrpHelpers.setDataByPath('altrppagestate.selected_obj', altrpHelpers.getDataByPath('altrpdata.obj_list')[{{altrpIndex}}]);
+RaiseActions('_SELECT_OBJ_');
